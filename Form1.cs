5747d27aca1cb5ff3452ee9834a21e5ed288430e
Bahattin614 <ayten_baha@hotmail.com>
2023-04-04 08:52:28 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hafta1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte vize, final;
+            float ort;
+
+            vize = Convert.ToByte( textBox1.Text);
+
+            final =Convert.ToByte( textBox2.Text);
+
+            ort = (float)(vize * 0.4 + final * 0.6);
+
+
+            label3.Text =Convert.ToString (ort);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not hesaplama sistemimiz");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
